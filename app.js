5a53ef5fddb13d694e8ab01454b25f49f7bdd459
ChrisBug <Christopher199348@gmail.com>
2018-01-12 18:11:26 +0000

    working on jwt add a very crude basic layout for angular

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import jwt from 'jsonwebtoken';
+import config from './config';
+import User from './models/user.model';
+import userRoutes from './routes/user.route';
+const app = express();
+// set the port
+const port = process.env.PORT || 8080;
+//Connect to mongoose
+mongoose.connect(config.database);
+
+//Allows crocs read up on best practice
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+  next();
+})
+//set up bodyParser
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+//set up morgan to Requests to console
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//Tester connection
+app.get('/', function(req, res){
+  res.send('Hello api is working');
+});
+
+app.use('/api/user', userRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h2 align=center>Page Not Found!</h2>');
+});
+
+// start the server
+app.listen(port,() => {
+  console.log(`App Server Listening at ${port}`);
+});
---
+++ models/user.model.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Set up the user model it is basic for now
+module.exports = mongoose.model('User', new Schema({
+  name: String,
+  password: String,
+  groupId: String
+}));
